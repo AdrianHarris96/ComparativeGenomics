6483c32f4a721f69589e8b3572c90e609c3df0a2
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2022-04-20 21:32:23 -0400

    KSNP Python Script
    KSNP script updated with additional argument, working directory.

--- ksnp.py
+++ ksnp.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-#Python Script Example: ./ksnp.py -g genomes -k 19 -o /home/groupb -c 8 -n -m
+#Python Script Example: ./ksnp.py -w /home/groupb -g genomes -k 19 -o /home/groupb/analysis/Team2-ComparativeGenomicsksnpk19_out -c 8 -n -m
 
 #Commands in the Shell: 
-#Step 1: MakeKSNP3infile <Genomes_dir> in_list A
-#Step 2: kSNP3 -in in_list -outdir /home/aharris334/ksnpk19_out -k 19 -CPU 8 -NJ -ML
+#Step 1: MakeKSNP3infile <genomes_dir> in_list A
+#Step 2: kSNP3 -in in_list -outdir <output_dir> -k <kmer> -CPU <num_of_cores> -NJ -ML
 
 #NOTE: The KSNP3 directory must be in your path and activate the conda environment, ksnp!
 
@@ -14,7 +14,8 @@ import time
 import os
 
 parser = ap.ArgumentParser()
-parser.add_argument("-g", "--genomes", help="directory containing genomes", required=True) #Be sure this in in your current working directory
+parser.add_argument("-w", "--workdir", help="working directory", required=True)
+parser.add_argument("-g", "--genomes", help="name of directory containing genomes", required=True) #Be sure this in in your current working directory
 parser.add_argument("-k", "--kmer", help="kmer count", required=True)
 parser.add_argument("-o", "--output", help="output directory", required=True)
 parser.add_argument("-c", "--cpu", help="number of cpus", required=True)
@@ -22,8 +23,11 @@ parser.add_argument("-n", "--neighbor", help="optional neighbor-joining tree", a
 parser.add_argument("-m", "--max", help="optional maximum-likelihood tree", action='store_true')
 args = parser.parse_args()
 
-makefile_run = sp.call(["MakeKSNP3infile", args.genomes, "in_list", "A"])
+os.chdir(args.workdir) #Shift to working directory
 
+makefile_run = sp.call(["MakeKSNP3infile", args.genomes, "in_list", "A"]) #Creation of a file with list of contigs and paths
+
+#KSNP Run Below:
 if args.neighbor and args.max:
 	ksnp_run = sp.call(["kSNP3", "-in", "in_list", "-outdir", args.output, "-k", args.kmer, "-CPU", args.cpu, "-NJ", "-ML"])
 elif args.neighbor:
